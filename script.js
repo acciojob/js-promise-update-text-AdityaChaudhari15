82680e4c9afd94f391d55dbfdcc0eede1aec8788
AdityaChaudhari15 <159461162+AdityaChaudhari15@users.noreply.github.com>
2025-10-27 16:54:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let output = document.getElementById('output');
+
+function xyz() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+xyz()
+  .then((message) => {
+    output.textContent = message;
+  })
+  .catch((err) => {
+    console.error(err);
+  });
